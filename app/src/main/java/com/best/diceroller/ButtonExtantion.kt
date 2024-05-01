85287c6a294f57c86316493c4eb0f01ae60462fa
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2024-05-01 23:41:11 +0600

    DiceRoller

---
+++ app/src/main/java/com/best/diceroller/ButtonExtantion.kt
@@ -0,0 +1,10 @@
+package com.best.diceroller
+
+import android.widget.Button
+
+fun Button.enable(){
+    this.isEnabled = true
+}
+fun Button.disable(){
+    this.isEnabled = false
+}
---
+++ app/src/main/java/com/best/diceroller/ResultActivity.kt
@@ -0,0 +1,18 @@
+package com.best.diceroller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.best.diceroller.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvResult.text = intent.getStringExtra("result")
+    }
+}
